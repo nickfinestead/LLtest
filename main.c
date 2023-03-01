a669b872a46f839fb368a5a0f69ac4e084ebaab3
nickfinestead <123410528+nickfinestead@users.noreply.github.com>
2023-03-01 16:10:35 -0600

    main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+      Code created for making a Singly Linked List
+      Comes with built in functions. Made for storing a command and an integer within a node.
+      
+        - void addNewNode(LL **head, char command[], int PID);
+        - void deleteOldestNode(LL **head);
+        - void printList(LL *head);
+        - void freeList(LL **head);
+
+
+*/
+#include <stdio.h>
+#include <stdlib.b>
+#include <string.h>
+
+
+int main()
+{
+  LL *head = NULL;
+  char *array[10] = {"cmd0", "cmd1", "cmd2", "cmd3", "cmd4", "cmd5", "cmd6", "cmd7", "cmd8", "cmd9"};
+  for(int i = 0; i < 10; i++)
+    addNewNode(&head, array[i], i);
+  printList(head);
+  freeList(&head);
+}
